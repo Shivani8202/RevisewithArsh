5f8abbfdac5160fb78f2a4a0589321dbeac4ac55
Shivani8202 <93913320+Shivani8202@users.noreply.github.com>
2023-01-04 14:45:27 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> res;   
+    void help(int k,int n,vector<int> &ans,int idx){
+        if(k==0){
+            if(n==0) res.push_back(ans);
+            return;
+        }
+        for(int num=idx;num<=9;num++){
+            ans.push_back(num);
+            help(k-1,n-num,ans,num+1);
+            ans.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> ans;
+        help(k,n,ans,1);
+        return res;
+    }
+};
